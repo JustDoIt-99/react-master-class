904653663ea6f28c3aadd1fd0ba09e9c72836304
sangyunpark99 <psangyun5108@gmail.com>
2026-04-25 19:29:20 +0900

    #2.4 Animations and Pesudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
-    display: flex;
+const Wrapper = styled.div`
+    display:flex;
 `;
 
-const Input = styled.input.attrs({required: true})`
+const rotationAnimation = keyframes`
+    0% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+    50% {
+        transform: rotate(360deg);
+        border-radius: 100px;
+    }
+    100% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+`;
+
+const Box = styled.div`
+    height: 200px;
+    width: 200px;
     background-color: tomato;
+    animation: ${rotationAnimation} 1s linear infinite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    span {
+        font-size: 36px;
+        
+        &:hover{
+            font-size: 40px;
+        }
+        
+        &:active {
+            opacity: 0;
+        }
+    }
 `;
 
 function App() {
   return (
-      <Father as="header">
-          <Input />
-          <Input />
-          <Input />
-          <Input />
-          <Input />
-          <Input />
-      </Father>
+    <Wrapper>
+        <Box>
+            <span>😒</span>
+        </Box>
+    </Wrapper>
   )
 }
 
