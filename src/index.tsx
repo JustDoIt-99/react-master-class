dc2f20fb6c7bba258280f66ce9b529d9d7466fd5
sangyunpark99 <sangyunpark99@gmail.com>
2026-04-26 18:50:03 +0900

    #4.1 BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {ThemeProvider} from "styled-components";
 import App from './App';
-import {lightTheme, darkTheme} from "./theme";
 
-const rootElement = document.getElementById('root');
-
-if(!rootElement) {
-    throw new Error("Root element not found");
-}
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
 
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={darkTheme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
